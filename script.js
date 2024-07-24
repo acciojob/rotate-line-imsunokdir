cc5073da2fa70381f17c876317ba81ec846a0649
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-24 18:06:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+//your JS code here. If required
+let line = document.getElementById("line");
+let deg = 0
+let times = document.getElementById("times");
+function rotate() {
+	deg = (deg+2)%180;
+	line.style.transform=`rotate(${deg}deg)`
+}
+
+const rotateLine = setInterval(rotate,20)
+
+
+
